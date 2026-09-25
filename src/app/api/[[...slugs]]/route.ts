1ec077d9a7b452daa51936ed522066ac6c997f64
EmonMridha <emonmridha712@gmail.com>
2026-09-25 21:46:39 +0600

    added another new middleware

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,6 +1,8 @@
 import { redis } from '@/lib/redis'
 import { Elysia, t } from 'elysia'
 import { nanoid } from 'nanoid'
+import { authMiddleware } from './auth'
+import { z } from 'zod'
 
 const ROOM_TTL_SECONDS = 60 * 10
 
@@ -15,10 +17,33 @@ const rooms = new Elysia({ prefix: '/room' })
             createdAt: Date.now(),
         })
 
+        const messages = new Elysia({ prefix: '/messages' }).use(authMiddleware)
+
         // deleting the room after 10 minutes
         await redis.expire(`meta: ${roomId}`, ROOM_TTL_SECONDS)
 
         return { roomId }
+    })
+
+const messages = new Elysia({ prefix: "/messages" }).use(authMiddleware).post('/',
+    async ({ body, auth }) => {
+        const { sender, text } = body
+        const { roomId } = auth
+
+        const roomExists = await redis.exists(`meta:${roomId}`)
+
+        if (!roomExists) {
+            throw new Error("Room Does not exists")
+        }
+
+
+    },
+    {
+        query: z.object({ roomId: z.string() }),
+        body: z.object({
+            sender: z.string().max(100),
+            text: z.string().max(100)
+        })
     })
 
 const app = new Elysia({ prefix: '/api' }).use(rooms)
